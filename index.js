2abe7b2b59a82197827a08434096d477626059fd
Destinee Saunders <dsaunders290@gmail.com>
2022-09-11 19:35:09 -0400

    commit tree 3a515b6013fcff1900e8a73072b5f6f9c6ac8fb2

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(sleep = 'roller-skate'){
+    return`This Saturday, I want to ${sleep}!`
+} 
+function mondayWork(work = 'go to the office'){
+    return `This Monday, I will ${work}.`
+}
+function wrapAdjective(Flair = '*'){
+   return function(adjective = 'special'){
+    return `You are ${Flair}${adjective}${Flair}!`
+   }
+    }
+
